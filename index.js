62d9f75fe5ae02fcc4133be5c2c0beb87637c97e
Sean Kelliher <sbkelliher@icloud.com>
2021-02-22 23:21:02 -0500

    Add 'index.js', etry file for new JS modules (coming next).

---
+++ index.js
@@ -0,0 +1,30 @@
+import {createElements} from "./app/elements.js";
+import {fillDetails} from "./app/details.js";
+import {clearButtons, highlightButton} from "./app/buttons.js";
+
+//After DOM loads...
+document.addEventListener("DOMContentLoaded", function () {
+    "use strict";
+
+    //Invoke these functions.
+    createElements();
+    fillDetails("birthplaceColor", "birthplace");
+    highlightButton("birthplace");
+
+    //Get the nav.
+    const nav = document.querySelector("nav");
+
+    //Monitor for clicks.
+    nav.addEventListener("click", function (event) {
+
+        //Get target, create arguments from it.
+        const etid = event.target.id;
+        const color = `${etid}Color`;
+        const text = `${etid}`;
+
+        //Invoke next functions.
+        fillDetails(color, text);
+        clearButtons();
+        highlightButton(etid);
+    });
+});
